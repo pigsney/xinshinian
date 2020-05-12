e91b9a8e068c74b12c65ef54b1c80f5cb5f41f8f
pigsney <1414607743@qq.com>
2020-05-12 23:43:09 +0800

    创建项目

---
+++ src/main/java/com/shop/xinshinian/XinshinianApplication.java
@@ -0,0 +1,13 @@
+package com.shop.xinshinian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinshinianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinshinianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/xinshinian/XinshinianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.xinshinian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinshinianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
